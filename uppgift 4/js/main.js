3eb1a22ceb0609f4a3d1e3f25d43678acd06f9f7
anna1gustavsson <74293920+anna1gustavsson@users.noreply.github.com>
2021-01-13 10:02:35 +0100

    Update main.js

--- uppgift 4/js/main.js
+++ uppgift 4/js/main.js
@@ -2,8 +2,6 @@
 
 
 
-
-
 let passSave = "ux";
 
 
@@ -37,7 +35,7 @@ function checkPass() {
 			return false;
 		} else {
 			//make the cookie
-			document.cookie = "username=Anna";
+			document.cookie = "username="+username;
 			
 			
 			console.log("make cookie");
