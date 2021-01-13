c951c1204a0c972b8f627afd654eb7a2ba744535
anna1gustavsson <74293920+anna1gustavsson@users.noreply.github.com>
2021-01-13 08:52:34 +0100

    komentarer

--- uppgift 4/js/main.js
+++ uppgift 4/js/main.js
@@ -60,12 +60,12 @@ function checkPass() {
 function makeCookie() {
 	// this is how you make a cookie
 	// its a way to save strings in for a "site"
-	document.cookie = "username=Anna";
+	//document.cookie = "username=Anna";
 	
 	// 
 	//	By default, the cookie is deleted when the browser is closed
 	// 	add the expires tag with a date to set how long the cookie show last
-	console.log("make cookie");
+	//console.log("make cookie");
 	//when you make a cookie it will stay there for that site, refreshing the page normally wont remove it.
 	// read more here: https://www.w3schools.com/js/js_cookies.asp
 }
